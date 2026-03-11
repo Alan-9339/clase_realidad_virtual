14a82a79ebc61ef0e102b1d7a294b5f1ec95add8
Alan-9339 <88732737+Alan-9339@users.noreply.github.com>
2026-03-11 10:05:54 -0600

    Practica2

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -4,23 +4,57 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
-    public GameObject model;
-    public Color color;
+    [Header("Modelos para Color")]
+    public GameObject brazoDerecho;
+    public GameObject mochila;
+    public GameObject gorro;
+
+    [Header("Accesorios (Cambio de Modelo)")]
+    public GameObject[] accesorios; 
+
+    [Header("Configuración")]
     public Material colorMaterial;
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
-        //model.GetComponent<Renderer>().material.color = Color.white;
+        DesactivarTodosLosAccesorios();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void ChangeColor_BTN()
     {
-        model.GetComponent<Renderer>().material.color = color;
-        colorMaterial.color = color;
+        Color randomColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        int indiceAleatorio = Random.Range(0, 3);
+
+        GameObject objetoSeleccionado = null;
+        switch (indiceAleatorio)
+        {
+            case 0: objetoSeleccionado = brazoDerecho; break;
+            case 1: objetoSeleccionado = mochila; break;
+            case 2: objetoSeleccionado = gorro; break;
+        }
+
+        if (objetoSeleccionado != null)
+            objetoSeleccionado.GetComponent<Renderer>().material.color = randomColor;
+    }
+
+    public void ChangeAccessory_BTN()
+    {
+        if (accesorios.Length == 0) return;
+
+        DesactivarTodosLosAccesorios();
+
+        int indiceAleatorio = Random.Range(0, accesorios.Length);
+        if (accesorios[indiceAleatorio] != null)
+        {
+            accesorios[indiceAleatorio].SetActive(true);
+        }
+    }
+
+    private void DesactivarTodosLosAccesorios()
+    {
+        foreach (GameObject acc in accesorios)
+        {
+            if (acc != null) acc.SetActive(false);
+        }
     }
 }
